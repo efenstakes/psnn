3237d1f4c98de2a46f1553239562a1bb42d996ca
Efen Stakes <efenstakes101@gmail.com>
2022-05-02 15:48:27 +0300

    leave speed as constant

--- scripts/BarrierBase.gd
+++ scripts/BarrierBase.gd
@@ -7,7 +7,8 @@ var speed: Vector2 = Vector2(0, 200)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	speed.y = rand_range(100, 300)
+	# speed.y = rand_range(100, 300)
+	pass
 
 
 func _physics_process(delta)-> void:
