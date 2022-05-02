4f708cf0f7c500cb89fef6199840e223c9083901
Efen Stakes <efenstakes101@gmail.com>
2022-05-02 22:04:08 +0300

    when new game starts, set score to 0 & update button texts after pause/resume

--- scripts/HUD.gd
+++ scripts/HUD.gd
@@ -9,7 +9,11 @@ onready var ScoreLabel := $ColorRect/HBoxContainer/Score
 func _ready():
 	pause_mode = Node.PAUSE_MODE_PROCESS
 	EventManager.connect("new_score", self, "_on_NewScore")
+	EventManager.connect("new_game", self, "_on_NewGame")
 
+
+func _on_NewGame(score):
+	ScoreLabel.text = str( 0 )
 
 func _on_NewScore(score):
 	ScoreLabel.text = str( score )
@@ -18,26 +22,26 @@ func _on_NewScore(score):
 func _on_Button_pressed():
 	if get_tree().paused:
 		GameManager.unpause_game()
-		$ColorRect/HBoxContainer/Button.text = "Resume" 
+		$ColorRect/HBoxContainer/Button.text = "Pause" 
 	else: 
 		GameManager.pause_game()
-		$ColorRect/HBoxContainer/Button.text = "Pause"
+		$ColorRect/HBoxContainer/Button.text = "Resume"
 		
-		
-func _process(delta):
-	if Input.is_action_just_pressed("ui_cancel"):
-		if get_tree().paused:
-			GameManager.unpause_game()
-		else:
-			GameManager.unpause_game()
-			
-func _unhandled_key_input(event):
-	if Input.is_action_just_pressed("ui_cancel"):
-		if get_tree().paused:
-			GameManager.unpause_game()
-		else:
-			GameManager.unpause_game()
-			
-	if get_tree().paused:
-		GameManager.unpause_game()
+#
+#func _process(delta):
+#	if Input.is_action_just_pressed("ui_cancel"):
+#		if get_tree().paused:
+#			GameManager.unpause_game()
+#		else:
+#			GameManager.unpause_game()
+#
+#func _unhandled_key_input(event):
+#	if Input.is_action_just_pressed("ui_cancel"):
+#		if get_tree().paused:
+#			GameManager.unpause_game()
+#		else:
+#			GameManager.unpause_game()
+#
+#	if get_tree().paused:
+#		GameManager.unpause_game()
 	
