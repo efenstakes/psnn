eedddb5220e645a1642b5b644643f1f438e0d0d3
Efen Stakes <efenstakes101@gmail.com>
2022-05-02 19:23:20 +0300

    update logic to deal with game over, start & new score events

--- autoloads/EventManager.gd
+++ autoloads/EventManager.gd
@@ -1,9 +1,17 @@
 extends Node
 
 
+signal new_game
 signal new_score
 signal gain_made
 signal player_died
+
+
+
+func new_game_event()-> void:
+	emit_signal("new_game")
+	emit_signal("new_score", 0)
+
 
 func new_score_event(score: int)-> void:
 	emit_signal("new_score", score)
--- autoloads/GameManager.gd
+++ autoloads/GameManager.gd
@@ -1,9 +1,44 @@
 extends Node
 
 
+var GameOverHUD
+
+
+var player_score : int = 0
+
+
 func _ready()-> void:
+	EventManager.connect("new_game", self, "_onNewGameStarted")
+	EventManager.connect("new_score", self, "_onNewScore")
 	EventManager.connect("player_died", self, "_onPlayerDied")
 
 
+
+func new_game()-> void:
+	EventManager.new_game_event()
+	get_tree().paused = false
+	
+	
+func pause_game()-> void:
+	get_tree().paused = true
+	
+	
+func unpause_game()-> void:
+	get_tree().paused = false
+	
+	
+	
+func _onNewGameStarted()-> void:
+	print("new game started  ")
+	player_score = 0
+	
+
+
+func _onNewScore(score: int)-> void:
+	print("new game score  ")
+	player_score = score
+	
+
 func _onPlayerDied()-> void:
 	print("player died")
+	get_tree().paused = true
