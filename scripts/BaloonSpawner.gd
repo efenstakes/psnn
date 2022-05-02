c9ad9e665c13da2ec051779b1afae34e8781b163
Efen Stakes <efenstakes101@gmail.com>
2022-05-02 18:05:06 +0300

    add more logic

--- scripts/BaloonSpawner.gd
+++ scripts/BaloonSpawner.gd
@@ -6,12 +6,26 @@ var BalloonScene : PackedScene = load("res://scenes/Balloon.tscn")
 # screen size
 var SCREEN_SIZE := Vector2.ZERO
 
+# score
+var score : int = 0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	SCREEN_SIZE = get_viewport_rect().size
 	randomize()
+	EventManager.connect("gain_made", self, "_on_GainMade")
+	print("b4 get_tree().create_timer(2)")
+	get_tree().create_timer(.5)
+	print("after get_tree().create_timer(2)")
+	EventManager.new_score_event(score)
 
+
+
+
+func _on_GainMade(gain):
+	score += gain
+	EventManager.new_score_event(score)
 
 
 func _on_Timer_timeout():
