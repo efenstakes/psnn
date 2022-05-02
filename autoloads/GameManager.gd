9d531dc5beb5a9cb5ad7c30dbd8603c097c331e8
Efen Stakes <efenstakes101@gmail.com>
2022-05-02 21:56:20 +0300

    pause game when player dies

--- autoloads/GameManager.gd
+++ autoloads/GameManager.gd
@@ -7,7 +7,9 @@ var GameOverHUD
 var player_score : int = 0
 
 
+# Called when the node enters the scene tree for the first time.
 func _ready()-> void:
+	pause_mode = Node.PAUSE_MODE_PROCESS
 	EventManager.connect("new_game", self, "_onNewGameStarted")
 	EventManager.connect("new_score", self, "_onNewScore")
 	EventManager.connect("player_died", self, "_onPlayerDied")
@@ -16,6 +18,7 @@ func _ready()-> void:
 
 func new_game()-> void:
 	EventManager.new_game_event()
+	# $HUDDER.visible = true
 	get_tree().paused = false
 	
 	
@@ -29,6 +32,7 @@ func unpause_game()-> void:
 	
 	
 func _onNewGameStarted()-> void:
+	unpause_game()
 	print("new game started  ")
 	player_score = 0
 	
@@ -41,4 +45,5 @@ func _onNewScore(score: int)-> void:
 
 func _onPlayerDied()-> void:
 	print("player died")
-	get_tree().paused = true
+	# $HUDDER.visible = false
+	pause_game()
