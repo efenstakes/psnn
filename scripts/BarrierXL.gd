6df7c50a26f21d43bdecbab5cb076a227ccfaf5c
Efen Stakes <efenstakes101@gmail.com>
2022-05-02 17:26:58 +0300

    update collision logic

--- scripts/BarrierXL.gd
+++ scripts/BarrierXL.gd
@@ -7,8 +7,8 @@ func _ready():
 	pass # Replace with function body.
 
 
-func _on_Area2D_body_entered(body: KinematicBody2D):
-	if body.is_in_group("Player"):
+func _on_Area2D_body_entered(body):
+	if body.name == "Player":
 		print("collided")
 		EventManager.player_died_event()
 		queue_free()
