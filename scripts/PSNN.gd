b89091b625b98cab5518726a80847a83a52ea616
Efen Stakes <efenstakes101@gmail.com>
2022-05-04 21:20:12 +0300

    remove logic that hides or shows Game Over HUD

--- scripts/PSNN.gd
+++ scripts/PSNN.gd
@@ -5,9 +5,6 @@ onready var GameOverHUD : = $GameOverHUD
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#pause_mode = Node.PAUSE_MODE_PROCESS
-	
-	GameOverHUD.visible  = false
 	EventManager.connect("new_game", self, "_onNewGameStarted")
 	EventManager.connect("player_died", self, "_onPlayerDied")
 
@@ -19,12 +16,9 @@ func _onNewGameStarted()-> void:
 	for item in get_tree().get_nodes_in_group("Balloon"):
 		item.queue_free()
 				
-	GameOverHUD.visible  = false
-	
-
+				
 func _onPlayerDied()-> void:
 	print("player died")
-	GameOverHUD.visible  = true
 	
 	
 	
