b9e3c13c78ae11d6555ae9a051b4235d7d026a16
Efen Stakes <efenstakes101@gmail.com>
2022-05-09 17:03:06 +0300

    add logic to blast a barrier if shield is active

--- scripts/BarrierXL.gd
+++ scripts/BarrierXL.gd
@@ -1,14 +1,39 @@
 extends "res://scripts/BarrierBase.gd"
 
 
+var shield_active : bool = false
+
+onready var Blaster := $CPUParticles2D
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	EventManager.connect("sheild_activated", self, "_onShieldActivated")
+	EventManager.connect("sheild_deactivated", self, "_onShieldDeactivated")
+
+
 
 
 func _on_Area2D_body_entered(body):
-	if body.name == "Player":
-		#print("collided")
+	if !shield_active and body.name == "Player":
 		EventManager.player_died_event()
-		# queue_free()
+		
+	if shield_active and body.name == "Player":
+		EventManager.gain_made_event(20)
+		$Area2D/Sprite.visible = false
+		Blaster.emitting = true
+		yield( get_tree().create_timer(3), "timeout" )
+		queue_free()
+
+
+
+
+func _onShieldActivated():
+	shield_active = true
+	
+	
+func _onShieldDeactivated():
+	yield( get_tree().create_timer(2), "timeout" )
+	shield_active = false
+	
