93957a8c7f1eb0d183737e4620c96ea0d870f85d
Efen Stakes <efenstakes101@gmail.com>
2022-05-09 19:54:52 +0300

    add logic to manage shield and its collider

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,6 +1,10 @@
 extends KinematicBody2D
 
 
+onready var ShieldCollider := $ShieldCollider
+onready var Shield := $Shield
+
+
 # direction
 export(Vector2) var direction : Vector2 = Vector2.ZERO
 
@@ -18,14 +22,22 @@ export(int) var acceleration := 4000
 export(Vector2) var SCREEN_SIZE := Vector2.ZERO
 
 
+var shield_active := false
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#print("player loaded")
 	SCREEN_SIZE = get_viewport().size
 	#print("size")
 	#print(SCREEN_SIZE)
+	
+	deactivate_shield()
+	
 	EventManager.connect("player_died", self, "_onPlayerDied")
 	EventManager.connect("new_game", self, "_onNewGame")
+	EventManager.connect("sheild_activated", self, "_onShieldActivated")
+	EventManager.connect("sheild_deactivated", self, "_onShieldDeactivated")
 
 
 func _physics_process(delta)-> void:
@@ -49,3 +61,24 @@ func _onNewGame()-> void:
 	
 func _onPlayerDied()-> void:
 	rotation = -320
+
+	
+func _onShieldActivated():
+	activate_shield()
+	
+	shield_active = true
+	
+func _onShieldDeactivated():
+	deactivate_shield()
+	
+	shield_active = false
+	
+	
+	
+func activate_shield():
+	ShieldCollider.disabled = false
+	Shield.visible = true
+	
+func deactivate_shield():
+	ShieldCollider.disabled = true
+	Shield.visible = false
